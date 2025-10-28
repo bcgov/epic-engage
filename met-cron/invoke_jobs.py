84ae61cb6ba997ca2ed7a0011d88ec185ff5a8c4
Shaelyn Tolkamp <46355612+tolkamps1@users.noreply.github.com>
2025-10-28 13:39:49 -0700

    [Engage-44-2] Fix docker file python version (#118)
    * [Engage-44-2] Fix docker file python version
    
    * [engage-44-2] update met-cron to python 3.12
    
    * update cron dockerfile

--- met-cron/invoke_jobs.py
+++ met-cron/invoke_jobs.py
@@ -66,29 +66,28 @@ def run(job_name):
     from tasks.subscription_mailer import SubscriptionMailer
     application = create_app()
 
-    application.app_context().push()
-
-    print('Requested Job:', job_name)
-    if job_name == 'ENGAGEMENT_CLOSEOUT':
-        MetEngagementCloseout.do_closeout()
-        application.logger.info(f'<<<< Completed MET Engagement Closeout >>>>')
-    elif job_name == 'ENGAGEMENT_PUBLISH':
-        MetEngagementPublish.do_publish()
-        application.logger.info(f'<<<< Completed MET Engagement Publish >>>>')
-    elif job_name == 'PURGE':
-        MetPurge.do_purge()
-        application.logger.info('<<<< Completed MET Purge >>>>')
-    elif job_name == 'COMMENT_REDACT':
-        MetCommentRedact.do_redact()
-        application.logger.info('<<<< Completed MET COMMENT_REDACT >>>>')
-    elif job_name == 'PUBLISH_EMAIL':
-        SubscriptionMailer.do_email()
-        application.logger.info('<<<< Completed MET PUBLISH_EMAIL >>>>')
-    elif job_name == 'CLOSING_SOON_EMAIL':
-        EngagementClosingSoonMailer.do_email()
-        application.logger.info('<<<< Completed MET CLOSING_SOON_EMAIL >>>>')
-    else:
-        application.logger.debug('No valid args passed.Exiting job without running any ***************')
+    with application.app_context():
+        print('Requested Job:', job_name)
+        if job_name == 'ENGAGEMENT_CLOSEOUT':
+            MetEngagementCloseout.do_closeout()
+            application.logger.info(f'<<<< Completed MET Engagement Closeout >>>>')
+        elif job_name == 'ENGAGEMENT_PUBLISH':
+            MetEngagementPublish.do_publish()
+            application.logger.info(f'<<<< Completed MET Engagement Publish >>>>')
+        elif job_name == 'PURGE':
+            MetPurge.do_purge()
+            application.logger.info('<<<< Completed MET Purge >>>>')
+        elif job_name == 'COMMENT_REDACT':
+            MetCommentRedact.do_redact()
+            application.logger.info('<<<< Completed MET COMMENT_REDACT >>>>')
+        elif job_name == 'PUBLISH_EMAIL':
+            SubscriptionMailer.do_email()
+            application.logger.info('<<<< Completed MET PUBLISH_EMAIL >>>>')
+        elif job_name == 'CLOSING_SOON_EMAIL':
+            EngagementClosingSoonMailer.do_email()
+            application.logger.info('<<<< Completed MET CLOSING_SOON_EMAIL >>>>')
+        else:
+            application.logger.debug('No valid args passed.Exiting job without running any ***************')
 
 
 if __name__ == "__main__":
--- met-cron/manage.py
+++ met-cron/manage.py
@@ -1,16 +1,15 @@
-from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
+from flask_migrate import Migrate
+from flask.cli import FlaskGroup
+
 import invoke_jobs
+from met_cron.models import db
 
-from src.met_cron.models import db
-
+# Create the app in migration mode
 app = invoke_jobs.create_app(run_mode='migration')
-# app.config.from_object(os.environ['APP_SETTINGS'])
-
 migrate = Migrate(app, db)
-manager = Manager(app)
 
-manager.add_command('db', MigrateCommand)
+# Create a Flask CLI group so `flask` commands work
+cli = FlaskGroup(app)
 
 if __name__ == '__main__':
-    manager.run()
+    cli()
