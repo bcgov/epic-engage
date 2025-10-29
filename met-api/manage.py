345985e0bcaeee092d4cba680ec5f57a63b5d645
Shaelyn Tolkamp <shaelyn.tolk@gmail.com>
2025-10-28 19:35:59 -0700

    [Engage-44] Fix go-crond and api docker entrypoint, gunicron

--- met-api/manage.py
+++ met-api/manage.py
@@ -4,7 +4,3 @@ from met_api import create_app, db
 app = create_app()
 migrate = Migrate(app, db)
 
-# Only needed if you want to expose extra custom commands
-# Otherwise, you can run Flask CLI commands directly
-if __name__ == "__main__":
-    app.run()
